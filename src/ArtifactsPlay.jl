e3c4cb68d983df20e975355376078ba9af9cf52d
Erik Engheim <erik.engheim@mac.com>
2021-01-29 23:40:45 +0100

    Some debug code

--- src/ArtifactsPlay.jl
+++ src/ArtifactsPlay.jl
@@ -5,6 +5,13 @@ export greet
 using Pkg.Artifacts
 
 function greet()
+    # hash = artifact_hash("foobar", "Artifacts.toml")
+    # if isnothing(hash)
+    #    println("Unable to lookup 'foobar' artifact")
+    #    if !artifact_exists(hash)
+    #       println("Artifact 'foobar' not yet installed")
+    #    end
+    # end
     path = joinpath(artifact"foobar", "msg.txt")
     println(read(path, String))
 end
